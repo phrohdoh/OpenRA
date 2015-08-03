53a75a620bd45de198be5034d05388139be72efa
Taryn Hill <Phrohdoh@gmail.com>
2015-08-03 13:09:45 -0500

    WIP TS IceLayer

---
+++ OpenRA.Mods.TS/Traits/World/IceLayer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.TS.Traits
+{
+	public class IceLayerInfo : ITraitInfo
+	{
+		public readonly int MaxStrength = int.MaxValue;
+
+		public object Create(ActorInitializer init) { return new IceLayer(init.Self, this); }
+	}
+
+	public class IceLayer
+	{
+		CellLayer<int> strength;
+
+		public IceLayer(Actor self, IceLayerInfo info)
+		{
+		}
+
+		public bool DamageCell(CPos cell, int damage)
+		{
+			if (!strength.Contains(cell))
+				return false;
+
+			if (strength[cell] -= damage <= 0)
+				OnCellDeath(cell);
+
+			return true;
+		}
+
+		void OnCellDeath(CPos cell)
+		{
+
+		}
+	}
+}
