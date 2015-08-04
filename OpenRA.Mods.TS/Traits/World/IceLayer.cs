a3576d64197dcdb123ca888007a04803ed15acfb
Taryn Hill <Phrohdoh@gmail.com>
2015-08-03 19:12:04 -0500

    WIP ice layer TS

--- OpenRA.Mods.TS/Traits/World/IceLayer.cs
+++ OpenRA.Mods.TS/Traits/World/IceLayer.cs
@@ -5,17 +5,26 @@ namespace OpenRA.Mods.TS.Traits
 {
 	public class IceLayerInfo : ITraitInfo
 	{
+		[Desc("Maximum damage each ice cell can take before breaking.")]
 		public readonly int MaxStrength = int.MaxValue;
+
+		[Desc("Ice regrows once per this many ticks.")]
+		public readonly int RegrowthDelay = 25;
 
 		public object Create(ActorInitializer init) { return new IceLayer(init.Self, this); }
 	}
 
-	public class IceLayer
+	public class IceLayer : ITick
 	{
+		readonly IceLayerInfo info;
+
 		CellLayer<int> strength;
+		int ticksSinceLastRegrowth;
 
 		public IceLayer(Actor self, IceLayerInfo info)
 		{
+			this.info = info;
+			ticksSinceLastRegrowth = info.RegrowthDelay;
 		}
 
 		public bool DamageCell(CPos cell, int damage)
@@ -32,6 +41,15 @@ namespace OpenRA.Mods.TS.Traits
 		void OnCellDeath(CPos cell)
 		{
 
+		}
+
+		public void Tick(Actor self)
+		{
+			if (--ticksSinceLastRegrowth <= 0)
+			{
+				ticksSinceLastRegrowth = info.RegrowthDelay;
+				// todo: regrowice
+			}
 		}
 	}
 }
