a77da0c59a9e992fb9c66db5c413ab91bd32ef8c
Taryn Hill <Phrohdoh@gmail.com>
2015-08-04 20:29:33 -0500

    Fix gactwr's inability to fire because AttackTurreted wasn't being activated

--- mods/ts/maps/gdi1a/map.lua
+++ mods/ts/maps/gdi1a/map.lua
@@ -1,23 +1,10 @@
 WorldLoaded = function()
-	local tw_up = "tower.vulcan"
+	local tw_up = "tower"
+	local tw_up_vulc = "tower.vulcan"
 
-	if tower1.HasProperty("AcceptsUpgrade") then
-		if tower1.AcceptsUpgrade(tw_up) then
-			tower1.GrantUpgrade(tw_up)
-		else
-			Media.DisplayMessage("t1 doesn't accept " .. tw_up)
-		end
-	else
-		Media.DisplayMessage("t1 doesn't accept upgrades")
-	end
+	tower1.GrantUpgrade(tw_up)
+	tower1.GrantUpgrade(tw_up_vulc)
+	tower2.GrantUpgrade(tw_up)
+	tower2.GrantUpgrade(tw_up_vulc)
 
-	if tower2.HasProperty("AcceptsUpgrade") then
-		if tower2.AcceptsUpgrade(tw_up) then
-			tower2.GrantUpgrade(tw_up)
-		else
-			Media.DisplayMessage("t2 doesn't accept " .. tw_up)
-		end
-	else
-		Media.DisplayMessage("t2 doesn't accept upgrades")
-	end
 end
