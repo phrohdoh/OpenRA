e0f6395ee9edc24ba328fcd0eebadcb926472994
Taryn Hill <Phrohdoh@gmail.com>
2015-08-05 20:40:56 -0500

    WIP GDI01 Add reinforcements via caryall

--- mods/ts/maps/gdi1a/map.lua
+++ mods/ts/maps/gdi1a/map.lua
@@ -1,4 +1,12 @@
+CarryallType = "trnsport"
+DropPath1 = { para_start.Location, para_end_1.Location }
+DropPath2 = { para_start.Location, para_end_2.Location }
+GDIReinforcements = { "e1", "e1", "smech" }
+
 WorldLoaded = function()
+	pPlayer = Player.GetPlayer("player")
+	pNod = Player.GetPlayer("Nod")
+
 	local tw_up = "tower"
 	local tw_up_vulc = "tower.vulcan"
 
@@ -7,4 +15,15 @@ WorldLoaded = function()
 	tower2.GrantUpgrade(tw_up)
 	tower2.GrantUpgrade(tw_up_vulc)
 
+	UserInterface.SetMissionText("Eliminate all Nod presence.", pPlayer.Color)
+
+	Trigger.AfterDelay(3, SendReinforcements)
+end
+
+SendReinforcements = function()
+	Reinforcements.ReinforceWithTransport(pPlayer, CarryallType,
+		GDIReinforcements, DropPath1, { para_start.Location })
+
+	Reinforcements.ReinforceWithTransport(pPlayer, CarryallType,
+		GDIReinforcements, DropPath2, { para_start.Location })
 end
