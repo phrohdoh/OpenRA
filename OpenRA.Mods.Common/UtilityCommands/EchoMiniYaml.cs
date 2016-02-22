2ae7627878bbf882722c3c472af84168485d7314
Taryn Hill <Phrohdoh@gmail.com>
2016-02-21 20:48:38 -0600

    Test case for keeping yaml comments

---
+++ OpenRA.Mods.Common/UtilityCommands/EchoMiniYaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OpenRA.Mods.Common.UtilityCommands
+{
+	public class EchoMiniYaml : IUtilityCommand
+	{
+		string IUtilityCommand.Name { get { return "--echo"; } }
+
+		void IUtilityCommand.Run(ModData modData, string[] args)
+		{
+			var filename = args[1];
+			var my = MiniYaml.FromFile(filename);
+			foreach (var line in my.ToLines(false))
+				Console.WriteLine(line);
+		}
+
+		bool IUtilityCommand.ValidateArguments(string[] args)
+		{
+			return args.Length == 2;
+		}
+	}
+}
