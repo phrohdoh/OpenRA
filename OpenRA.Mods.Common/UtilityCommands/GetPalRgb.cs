3e128ac9cb63e09ef26bf0922512f546ff241245
Taryn Hill <Phrohdoh@gmail.com>
2016-03-10 13:25:54 -0600

    Add a util command to print out the rgb values for a given palette file

---
+++ OpenRA.Mods.Common/UtilityCommands/GetPalRgb.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using OpenRA.Graphics;
+
+namespace OpenRA.Mods.Common
+{
+	public class GetPalRgbCommand : IUtilityCommand
+	{
+		string IUtilityCommand.Name { get { return "--pal-rgb"; } }
+
+		void IUtilityCommand.Run(ModData modData, string[] args)
+		{
+			var filename = args[1];
+			var pal = new ImmutablePalette(filename, new int[0]);
+			for (var i = 0; i < 255; i++)
+			{
+				var c = pal.GetColor(i);
+				Console.WriteLine("{{r: {0}, g: {1}, b: {2}}}", c.R, c.G, c.B);
+			}
+		}
+
+		bool IUtilityCommand.ValidateArguments(string[] args)
+		{
+			return args.Length == 2;
+		}
+	}
+}
