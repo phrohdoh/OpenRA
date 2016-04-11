3c8f4ec46310cf5c786baa9d1038a2e4c126a941
Taryn Hill <Phrohdoh@gmail.com>
2016-04-10 19:50:47 -0500

    WIP elevation brush, just moves tile straight up and down

---
+++ OpenRA.Mods.Common/EditorBrushes/EditorElevationModifierBrush.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using OpenRA.Mods.Common.Widgets;
+using OpenRA.Graphics;
+
+namespace OpenRA.Mods.Common.Widgets
+{
+	public class EditorElevationModifierBrush : IEditorBrush
+	{
+		World world;
+		WorldRenderer wr;
+
+		public EditorElevationModifierBrush(EditorViewportControllerWidget editorWidget, WorldRenderer wr)
+		{
+			world = wr.World;
+			this.wr = wr;
+		}
+
+		void IEditorBrush.Tick() { }
+		void IDisposable.Dispose() { }
+
+		bool IEditorBrush.HandleMouseInput(MouseInput mi)
+		{
+			if (mi.Event != MouseInputEvent.Down)
+				return false;
+
+			var map = world.Map;
+			var cell = wr.Viewport.ViewToWorld(mi.Location);
+
+			var mapHeight = map.Height;
+			var currHeight = mapHeight[cell];
+
+			var whichClick = mi.Button == MouseButton.Left ? 1 : -1;
+
+			mapHeight[cell] = (byte)((currHeight + whichClick).Clamp(0, map.Grid.MaximumTerrainHeight));
+			return true;
+		}
+	}
+}
---
+++ OpenRA.Mods.Common/Widgets/Logic/Editor/MapEditorViewportLogic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using OpenRA.Graphics;
+using OpenRA.Widgets;
+
+namespace OpenRA.Mods.Common.Widgets.Logic
+{
+	public class MapEditorViewportLogic : ChromeLogic
+	{
+		EditorViewportControllerWidget vc;
+
+		[ObjectCreator.UseCtor]
+		public MapEditorViewportLogic(Widget widget, World world, WorldRenderer worldRenderer)
+		{
+			vc = widget as EditorViewportControllerWidget;
+
+			var btnDefaultBrush = widget.Get<ButtonWidget>("DEFAULT_BRUSH");
+			btnDefaultBrush.OnClick += vc.ClearBrush;
+
+			var btnElevationBrush = widget.GetOrNull<ButtonWidget>("ELEVATION_BRUSH");
+			if (btnElevationBrush != null)
+			{
+				btnElevationBrush.OnClick += () => { vc.SetBrush(new EditorElevationModifierBrush(vc, worldRenderer)); };
+			}
+		}
+	}
+}
