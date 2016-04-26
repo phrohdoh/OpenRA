fd856d2b053fceaa0a8c2d9dcee463a651ceab0e
Taryn Hill <Phrohdoh@gmail.com>
2016-04-26 10:14:07 -0500

    Add --list-lint-passes utility command

---
+++ OpenRA.Mods.Common/UtilityCommands/ListLintPassesCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.Common.UtilityCommands
+{
+	public class ListLintPassesCommand : IUtilityCommand
+	{
+		string IUtilityCommand.Name { get { return "--list-lint-passes"; } }
+
+		void IUtilityCommand.Run(ModData modData, string[] args)
+		{
+			var lintPassTypes = modData.ObjectCreator.GetTypesImplementing<ILintPass>().ToArray();
+			var lintRulePassTypes = modData.ObjectCreator.GetTypesImplementing<ILintRulesPass>().ToArray();
+			var lintMapPassTypes = modData.ObjectCreator.GetTypesImplementing<ILintMapPass>().ToArray();
+			var allLintTypes = lintPassTypes.Append(lintRulePassTypes).Append(lintMapPassTypes);
+
+			foreach (var type in allLintTypes)
+				Console.WriteLine(type.Name);
+		}
+
+		bool IUtilityCommand.ValidateArguments(string[] args) { return true; }
+	}
+}
