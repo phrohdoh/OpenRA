5cada1b0e2b585e42563da6d286b78e1744d852d
Taryn Hill <Phrohdoh@gmail.com>
2016-05-03 14:19:38 -0500

    Add YamlToXml utility command

---
+++ OpenRA.Mods.Common/UtilityCommands/YamlToXmlCommand.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Xml;
+using System.Text;
+
+namespace OpenRA.Mods.Common.UtilityCommands
+{
+	public class YamlToXmlCommand : IUtilityCommand
+	{
+		string IUtilityCommand.Name { get { return "--yaml-to-xml"; } }
+
+		void IUtilityCommand.Run(ModData modData, string[] args)
+		{
+			var filename = args[1];
+			var yaml = MiniYaml.FromFile(filename);
+
+			var node0 = yaml[0];
+			Console.WriteLine(NodeToXml(node0));
+		}
+
+		string NodeToXml(MiniYamlNode node)
+		{
+			var k = node.Key;
+			var v = node.Value.Value;
+
+			var sb = new StringBuilder();
+			var writer = XmlWriter.Create(sb, new XmlWriterSettings { Indent = true, OmitXmlDeclaration = true });
+
+			writer.WriteStartElement("Node");
+			writer.WriteElementString("Key", k);
+			writer.WriteElementString("Value", v);
+
+			foreach (var c in node.Value.Nodes)
+			{
+				writer.WriteStartElement("Child");
+				writer.WriteRaw("\n");
+				writer.WriteRaw(NodeToXml(c));
+				writer.WriteRaw("\n");
+				writer.WriteEndElement();
+			}
+
+			writer.WriteEndElement();
+			writer.Close();
+
+			return sb.ToString();
+		}
+
+		bool IUtilityCommand.ValidateArguments(string[] args)
+		{
+			return args.Length == 2;
+		}
+	}
+}
