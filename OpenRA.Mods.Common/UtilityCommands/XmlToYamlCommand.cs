0e45835566cc5654067403352fbad760185b33f2
Taryn Hill <Phrohdoh@gmail.com>
2016-05-04 13:12:46 -0500

    Add xml-to-yaml and yaml-to-xml utility commands

---
+++ OpenRA.Mods.Common/UtilityCommands/XmlToYamlCommand.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+
+namespace OpenRA.Mods.Common.UtilityCommands
+{
+	public class XmlToYamlCommand : IUtilityCommand
+	{
+		string IUtilityCommand.Name { get { return "--xml-to-yaml"; } }
+		bool IUtilityCommand.ValidateArguments(string[] args) { return args.Length == 2; }
+
+		[Desc("PATH/TO/FILE.xml", "Convert an XML document to a YAML document, written to stdout.")]
+		void IUtilityCommand.Run(ModData modData, string[] args)
+		{
+			var filename = args[1];
+			var doc = new XmlDocument();
+
+			try
+			{
+				doc.Load(filename);
+			}
+			catch (XmlException)
+			{
+				doc.LoadXml("<root>" + File.ReadAllText(filename) + "</root>");
+			}
+
+			var root = doc.SelectSingleNode("root");
+			if (root == null)
+			{
+				Console.WriteLine("Root node's name must be 'root'. Found '{0}' instead.", doc.DocumentElement.Name);
+				Environment.Exit(1);
+			}
+
+			var topLevelNodes = root.SelectNodes("Node");
+
+			var sb = new StringBuilder();
+
+			foreach (XmlNode child in topLevelNodes)
+				sb.Append(RecurseNode(child, 0));
+
+			Console.WriteLine(sb.ToString());
+		}
+
+		string RecurseNode(XmlNode node, int depth)
+		{
+			var sb = new StringBuilder();
+
+			var key = node.SelectSingleNode("Key");
+			var value = node.SelectSingleNode("Value");
+			var children = node.SelectNodes("Child/Node");
+
+			var str = new string('\t', depth);
+
+			if (!string.IsNullOrWhiteSpace(key.InnerText))
+				str += key.InnerText + ":";
+
+			if (!string.IsNullOrWhiteSpace(value.InnerText))
+				str += " " + value.InnerText;
+
+			/* TODO: Once comments are save this can be added too.
+			var comment = node.SelectSingleNode("Comment");
+			if (comment != null && !string.IsNullOrWhiteSpace(comment.InnerText))
+				str += "# " + comment.InnerText;
+			*/
+
+			if (!string.IsNullOrWhiteSpace(str))
+				sb.AppendLine(str);
+
+			foreach (XmlNode c in children)
+				sb.Append(RecurseNode(c, depth + 1));
+
+			return sb.ToString();
+		}
+	}
+}
---
+++ OpenRA.Mods.Common/UtilityCommands/YamlToXmlCommand.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+using System.Xml;
+
+namespace OpenRA.Mods.Common.UtilityCommands
+{
+	public class YamlToXmlCommand : IUtilityCommand
+	{
+		string IUtilityCommand.Name { get { return "--yaml-to-xml"; } }
+		bool IUtilityCommand.ValidateArguments(string[] args) { return args.Length == 2; }
+
+		[Desc("PATH/TO/FILE.yaml", "Convert a YAML document to an XML document, written to stdout.")]
+		void IUtilityCommand.Run(ModData modData, string[] args)
+		{
+			var filename = args[1];
+			var yaml = MiniYaml.FromFile(filename);
+
+			var sb = new StringBuilder();
+			var writer = XmlWriter.Create(sb, new XmlWriterSettings { Indent = true, OmitXmlDeclaration = true });
+
+			writer.WriteStartElement("root");
+			writer.WriteRaw("\n");
+
+			foreach (var topLevelNode in yaml)
+				writer.WriteRaw(NodeToXml(topLevelNode) + "\n");
+
+			writer.WriteEndElement();
+			writer.Close();
+
+			Console.WriteLine(sb);
+		}
+
+		string NodeToXml(MiniYamlNode node)
+		{
+			var key = node.Key;
+			var value = node.Value.Value;
+
+			// TODO: Once comments are save this can be added too.
+			//var comment = node.Comment;
+
+			var sb = new StringBuilder();
+			var writer = XmlWriter.Create(sb, new XmlWriterSettings { Indent = true, OmitXmlDeclaration = true });
+
+			writer.WriteStartElement("Node");
+			writer.WriteElementString("Key", key);
+			writer.WriteElementString("Value", value);
+
+			foreach (var c in node.Value.Nodes)
+			{
+				writer.WriteStartElement("Child");
+				writer.WriteRaw("\n");
+				writer.WriteRaw(NodeToXml(c));
+				writer.WriteRaw("\n");
+				writer.WriteEndElement();
+			}
+
+			writer.WriteEndElement();
+			writer.Close();
+
+			return sb.ToString();
+		}
+	}
+}
