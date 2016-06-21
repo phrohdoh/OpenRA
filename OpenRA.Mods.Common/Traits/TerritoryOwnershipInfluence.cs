5e7a499d75a4ca5e5a6bd28f2da8db67e6fd0992
Taryn Hill <Phrohdoh@gmail.com>
2016-06-20 23:45:08 -0500

    WIP territory ownership system

---
+++ OpenRA.Mods.Common/Traits/TerritoryOwnershipInfluence.cs
@@ -0,0 +1,71 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2007-2016 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using OpenRA.Primitives;
+using OpenRA.Traits;
+using System.Linq;
+
+namespace OpenRA.Mods.Common.Traits
+{
+	public class TerritoryOwnershipInfluenceInfo : ITraitInfo
+	{
+		[FieldLoader.Require]
+		public readonly int Range;
+
+		public object Create(ActorInitializer init) { return new TerritoryOwnershipInfluence(init, this); }
+	}
+
+	public class TerritoryOwnershipInfluence : INotifyCreated, INotifyKilled, IRadarSignature
+	{
+		readonly TerritoryOwnershipManager manager;
+
+		Actor self;
+		TerritoryOwnershipInfluenceInfo info;
+		Player owner { get { return self.Owner; } }
+
+		public TerritoryOwnershipInfluence(ActorInitializer init, TerritoryOwnershipInfluenceInfo info)
+		{
+			self = init.Self;
+			this.info = info;
+
+			manager = init.World.WorldActor.TraitOrDefault<TerritoryOwnershipManager>();
+			if (manager == null)
+				throw new InvalidDataException("The World actor must have the TerritoryOwnershipManager trait to use TerritoryOwnershipInfluence.");
+		}
+
+		IEnumerable<CPos> GetInfluenceCells()
+		{
+			return self.World.Map.FindTilesInCircle(self.Location, info.Range, false);
+		}
+
+		void INotifyKilled.Killed(Actor self, AttackInfo e)
+		{
+			manager.ClearValue(owner, self.Location);
+		}
+
+		void INotifyCreated.Created(Actor self)
+		{
+			self.World.AddFrameEndTask(world => {
+				manager.UpdateValue(owner, GetInfluenceCells(), 1);
+			});
+		}
+
+		IEnumerable<Pair<CPos, Color>> IRadarSignature.RadarSignatureCells(Actor self)
+		{
+			foreach (var cell in GetInfluenceCells())
+				yield return Pair.New(cell, self.Owner.Color.RGB);
+		}
+	}
+}
---
+++ OpenRA.Mods.Common/Traits/World/TerritoryOwnershipManager.cs
@@ -0,0 +1,121 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2007-2016 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using System.Collections.Generic;
+using OpenRA.Traits;
+using System.Linq;
+using System.IO;
+
+namespace OpenRA.Mods.Common.Traits
+{
+	public class TerritoryOwnershipManagerInfo : ITraitInfo
+	{
+		public readonly string InitialOwnerInternalName = "Neutral";
+
+		public object Create(ActorInitializer init) { return new TerritoryOwnershipManager(init, this); }
+	}
+
+	public class TerritoryOwnershipManager : INotifyCreated
+	{
+		readonly Dictionary<Player, CellLayer<int>> ownershipValues;
+		readonly TerritoryOwnershipManagerInfo info;
+		//Player initialOwnerOfAllCells;
+
+		public TerritoryOwnershipManager(ActorInitializer init, TerritoryOwnershipManagerInfo info)
+		{
+			ownershipValues = new Dictionary<Player, CellLayer<int>>();
+			this.info = info;
+		}
+
+		public void UpdateValue(Player ofPlayer, CPos location, int delta)
+		{
+			CellLayer<int> values;
+			if (!ownershipValues.TryGetValue(ofPlayer, out values))
+			{
+				values = new CellLayer<int>(ofPlayer.World.Map);
+				ownershipValues.Add(ofPlayer, values);
+			}
+
+			values[location] += delta;
+			values[location] = values[location].Clamp(0, int.MaxValue);
+		}
+
+		public void UpdateValue(Player ofPlayer, IEnumerable<CPos> locations, int delta)
+		{
+			CellLayer<int> values;
+			if (!ownershipValues.TryGetValue(ofPlayer, out values))
+			{
+				values = new CellLayer<int>(ofPlayer.World.Map);
+				ownershipValues.Add(ofPlayer, values);
+			}
+
+			foreach (var location in locations)
+			{
+				values[location] += delta;
+				values[location] = values[location].Clamp(0, int.MaxValue);
+			}
+		}
+
+		public void ClearValue(Player ofPlayer, CPos location)
+		{
+			CellLayer<int> values;
+			if (!ownershipValues.TryGetValue(ofPlayer, out values))
+				return;
+
+			values[location] = 0;
+		}
+
+		public int GetValue(Player ofPlayer, CPos location)
+		{
+			CellLayer<int> values;
+			if (!ownershipValues.TryGetValue(ofPlayer, out values))
+				return -1;
+
+			return values[location];
+		}
+
+		public Player GetOwningPlayer(CPos ofLocation)
+		{
+			var lastValue = 0;
+			Player ret = null;
+
+			foreach (var kv in ownershipValues)
+			{
+				var value = kv.Value[ofLocation];
+				if (value > lastValue)
+				{
+					lastValue = value;
+					ret = kv.Key;
+				}
+			}
+
+			return ret;
+		}
+
+		void INotifyCreated.Created(Actor self)
+		{
+			self.World.AddFrameEndTask(world => {
+				var initialOwner = world.Players.FirstOrDefault(p => p.InternalName == info.InitialOwnerInternalName);
+				if (initialOwner == null)
+					throw new InvalidDataException("No player with InternalName '{0}' could be found.".F(info.InitialOwnerInternalName));
+
+				var defaultValues = new CellLayer<int>(world.Map);
+
+				// The default player must be the initial result of `GetOwningPlayer` for all cells.
+				// As soon as another player stakes a claim they will be the owner.
+				defaultValues.Clear(1);
+
+				//initialOwnerOfAllCells = initialOwner;
+				ownershipValues.Add(initialOwner, defaultValues);
+			});
+		}
+	}
+}
