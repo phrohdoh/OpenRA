6a949f9e0b6004e30bc97d0e390c7cabb243d805
Taryn Hill <Phrohdoh@gmail.com>
2016-07-01 18:01:05 -0500

    WIP DistanceToTerrain debug overlay

---
+++ OpenRA.Mods.Common/Traits/DistanceToTerrainDebugOverlay.cs
@@ -0,0 +1,44 @@
+﻿using OpenRA.Graphics;
+using OpenRA.Mods.Common.Graphics;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.Common.Traits
+{
+	public class DistanceToTerrainDebugOverlayInfo : ITraitInfo, Requires<IOccupySpaceInfo>
+	{
+		object ITraitInfo.Create(ActorInitializer init) => new DistanceToTerrainDebugOverlay(init.Self, this);
+	}
+
+	public class DistanceToTerrainDebugOverlay : IPostRender
+	{
+		readonly DistanceToTerrainDebugOverlayManager manager;
+		readonly RgbaColorRenderer renderer;
+		readonly Map map;
+
+		public DistanceToTerrainDebugOverlay(Actor self, DistanceToTerrainDebugOverlayInfo info)
+		{
+			renderer = Game.Renderer.WorldRgbaColorRenderer;
+			map = self.World.Map;
+
+			manager = self.World.WorldActor.TraitOrDefault<DistanceToTerrainDebugOverlayManager>();}
+
+		void IPostRender.RenderAfterWorld(WorldRenderer wr, Actor self)
+		{
+			if (manager == null || !manager.Enabled)
+				return;
+
+			var pos = self.CenterPosition;
+			var dat = map.DistanceAboveTerrain(pos);
+
+			var dist = dat.Length;
+			if (dist == 0)
+				return;
+
+			var vec = new WVec(0, 0, -dist);
+			var ground = pos + vec;
+
+			renderer.DrawLine(wr.ScreenPosition(pos), wr.ScreenPosition(ground), 1f, self.Owner.Color.RGB);
+			new TextRenderable(manager.Font, ground, 1, self.Owner.Color.RGB, dist.ToString()).Render(wr);
+		}
+	}
+}
---
+++ OpenRA.Mods.Common/Traits/World/DistanceToTerrainDebugOverlayManager.cs
@@ -0,0 +1,49 @@
+﻿using OpenRA.Graphics;
+using OpenRA.Mods.Common.Commands;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.Common.Traits
+{
+	public class DistanceToTerrainDebugOverlayManagerInfo : ITraitInfo
+	{
+		[Desc("The font used to render the world units above/below terrain.",
+			"Should match the value as-is in the Fonts section of the mod manifest (do not convert to lowercase).")]
+		public readonly string Font = "Small";
+
+		object ITraitInfo.Create(ActorInitializer init) => new DistanceToTerrainDebugOverlayManager(init.Self, this);
+	}
+
+	public class DistanceToTerrainDebugOverlayManager : INotifyCreated, IChatCommand
+	{
+		const string CommandName = "dist-terrain-overlay";
+		const string CommandHelp = "Displays distance above/below terrain.";
+
+		public readonly SpriteFont Font;
+		public bool Enabled;
+
+		public DistanceToTerrainDebugOverlayManager(Actor self, DistanceToTerrainDebugOverlayManagerInfo info)
+		{
+			if (!Game.Renderer.Fonts.TryGetValue(info.Font, out Font))
+				throw new YamlException("Could not find font '{0}'".F(info.Font));
+		}
+
+		void INotifyCreated.Created(Actor self)
+		{
+			// This lives on the world actor.
+			var console = self.TraitOrDefault<ChatCommands>();
+			var help = self.TraitOrDefault<HelpCommand>();
+			
+			if (console == null || help == null)
+				return;
+			
+			console.RegisterCommand(CommandName, this);
+			help.RegisterHelp(CommandName, CommandHelp);
+		}
+
+		void IChatCommand.InvokeCommand(string command, string arg)
+		{
+			if (command == CommandName)
+				Enabled ^= true;
+		}
+	}
+}
